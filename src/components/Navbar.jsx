58fe97a98787b4cf43dd0d67eb49b0effdd6dab0
luxndecibels <luxndecibelsdigi@gmail.com>
2026-07-29 22:02:37 +0530

    Update logo image in Navbar component
    to nave bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar() {
     <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <div className="nav-inner">
         <a className="nav-logo" onClick={() => scrollTo('home')}>
-          <img src="/logo-transparent.png" alt="Lux & Decibels" className="logo-img" />
+          <img src="/L&D%20LOGO%20WHITE.jpg" alt="Lux & Decibels" className="logo-img" />
         </a>
         <ul className="nav-links">
           {navLinks.map(link => (
